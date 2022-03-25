ab0b36437b8852e6f7c5082cdf575c83531ddd96
NahlaGalal <nahlaglal@gmail.com>
2022-03-25 21:55:00 +0200

    Setup application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    main: "#222328",
+    white: "#FFFFFF",
+    secondary: "#FDAF53",
+    background: "rgba(255, 255, 255, 0.3)",
+    green: "#1C5C5B",
+  },
+};
+
+const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
