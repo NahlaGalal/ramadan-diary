d818fb5618e0d5b899fcb328284cf8660d7e8bbe
NahlaGalal <nahlaglal@gmail.com>
2022-03-25 22:26:17 +0200

    Add home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,42 @@
-import { Box } from "@chakra-ui/react";
+import { Flex, Link as StyledLink } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import background from "../public/images/home.jpg";
+import Image from "next/image";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
-    <Box bgColor="var(--chakra-colors-brand-main)">
-      
-    </Box>
+    <Flex
+      bgColor="var(--chakra-colors-brand-main)"
+      height={"100vh"}
+      flexDirection={"column"}
+      alignItems="center"
+    >
+      <Image
+        src={background}
+        alt="Ramadan background"
+        objectFit="cover"
+        width={"600px"}
+      />
+      <Link href={"/"} passHref={true}>
+        <StyledLink
+          mt="auto"
+          mb={6}
+          bg="var(--chakra-colors-brand-green)"
+          border={"1px solid var(--chakra-colors-brand-green)"}
+          py={3}
+          px={10}
+          fontSize={"lg"}
+          zIndex={"docked"}
+          borderRadius={"md"}
+          fontWeight="500"
+          _hover={{ bg: "transparent" }}
+          _active={{ bg: "tranparent" }}
+        >
+          دخول
+        </StyledLink>
+      </Link>
+    </Flex>
   );
 };
 
