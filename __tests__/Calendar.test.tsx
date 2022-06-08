4bfd950263b7f07f3723bfaac17eef754358f94f
NahlaGalal <nahlaglal@gmail.com>
2022-06-08 02:52:16 +0200

    Add tests to calendar page

---
+++ __tests__/Calendar.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import Calendar from "../pages/Calendar";
+import { weekDays } from "../pages/Calendar/utils";
+
+
+test("should render Calendar page correctly", () => {
+  render(<Calendar />);
+
+  // Check for heading exist
+  const heading = screen.getByRole("heading", {
+    name: /ramadan daily/i,
+  });
+
+  expect(heading).toBeInTheDocument();
+
+  // Heading of table 
+  const tableHeadingCells = screen.getAllByRole("columnheader");
+  
+  expect(tableHeadingCells).toHaveLength(7);
+  tableHeadingCells.map((cell, i) => (
+    expect(cell).toHaveTextContent(weekDays[i])
+  ))
+
+  // Number of rows
+  const tableRows = screen.getAllByRole("row");
+
+  expect(tableRows).toHaveLength(6);
+
+  // Number of cells
+  const tableCells = screen.getAllByRole("gridcell");
+
+  expect(tableCells).toHaveLength(30);
+});
