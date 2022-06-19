d661c4884e88aae5e4b22e4eca257e30a409bba2
NahlaGalal <nahlaglal@gmail.com>
2022-06-19 05:23:44 +0200

    Add toggle password input test

---
+++ __tests__/Password.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import FormInput from "../components/FormInput";
+
+const user = userEvent.setup();
+
+test("should show / hide password input successfull", async () => {
+  render(
+    <FormInput
+      id="password"
+      label="password"
+      placeholder="password"
+      type="password"
+    />
+  );
+
+  const passwordInput = screen.getByLabelText("password");
+  const toggleBtn = screen.getByRole("button");
+
+  // Password input
+  expect(passwordInput).toHaveAttribute("type", "password");
+
+  await user.click(toggleBtn);
+
+  // Text input
+  expect(passwordInput).toHaveAttribute("type", "text");
+});
