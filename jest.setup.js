965ce28fa0b111b1770a420e1bc54b750eaccc44
NahlaGalal <nahlaglal@gmail.com>
2022-06-22 05:43:05 +0200

    Add server mocks to jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,11 @@
 import "@testing-library/jest-dom/extend-expect";
+import { server } from "./__mocks__/server";
+
+// Enable API mocking before tests.
+beforeAll(() => server.listen());
+
+// Reset any runtime request handlers we may add during the tests.
+afterEach(() => server.resetHandlers());
+
+// Disable API mocking after the tests are done.
+afterAll(() => server.close());
