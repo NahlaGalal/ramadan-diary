5bd1202477921ac5949ccc9a4783b21209f351f8
NahlaGalal <nahlaglal@gmail.com>
2022-06-22 05:54:42 +0200

    Add login mock

--- __mocks__/signup.ts
+++ __mocks__/signup.ts
@@ -2,6 +2,7 @@ import { rest } from "msw";
 import { EMAIL_REGEX } from "../utils/validations";
 
 export const signupHandlers = [
+  // Signup
   rest.post("/api/signup", (req, res, ctx) => {
     const data: {
       name: string;
@@ -24,6 +25,26 @@ export const signupHandlers = [
       errors.push({ confirm_password: "يجب أن لا تقل كلمة المرور عن 8 أحرف" });
     if (data.confirm_password !== data.password)
       errors.push({ confirm_password: "كلمتا المرور غير متطابقتان" });
+
+    if (errors.length) return res(ctx.status(400), ctx.json({ errors }));
+    else return res(ctx.status(200), ctx.json({ success: true }));
+  }),
+
+  // Login
+  rest.post("/api/login", (req, res, ctx) => {
+    const data: {
+      email: string;
+      password: string;
+    } = req.body as any;
+
+    let errors = [];
+
+    if (!data.email.length)
+      errors.push({ email: "يجب كتابة البريد اﻹلكتروني قبل التسجيل" });
+    if (!EMAIL_REGEX.test(data.email))
+      errors.push({ email: "يجب كتابة بريد إلكتروني صحيح" });
+    if (data.password.length < 8)
+      errors.push({ password: "يجب أن لا تقل كلمة المرور عن 8 أحرف" });
 
     if (errors.length) return res(ctx.status(400), ctx.json({ errors }));
     else return res(ctx.status(200), ctx.json({ success: true }));
